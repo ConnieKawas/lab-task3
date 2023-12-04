22c867f91d1d71878e8c9d2e16f823566b8bd572
ConnieKawas <conniekawas0209@gmail.com>
2023-12-04 08:35:36 +0800

    first commit

---
+++ LabTask3Array/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.BtnUpdate = New System.Windows.Forms.Button()
+        Me.BtnAdd = New System.Windows.Forms.Button()
+        Me.BtnView = New System.Windows.Forms.Button()
+        Me.BtnDelete = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.ListboxApps = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.SeaGreen
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(127, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(397, 43)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Simple App Management System"
+        '
+        'BtnExit
+        '
+        Me.BtnExit.BackColor = System.Drawing.Color.Red
+        Me.BtnExit.Font = New System.Drawing.Font("Microsoft YaHei", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnExit.Location = New System.Drawing.Point(584, 459)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(75, 44)
+        Me.BtnExit.TabIndex = 1
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = False
+        '
+        'BtnUpdate
+        '
+        Me.BtnUpdate.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.BtnUpdate.Location = New System.Drawing.Point(227, 361)
+        Me.BtnUpdate.Name = "BtnUpdate"
+        Me.BtnUpdate.Size = New System.Drawing.Size(75, 32)
+        Me.BtnUpdate.TabIndex = 2
+        Me.BtnUpdate.Text = "Update"
+        Me.BtnUpdate.UseVisualStyleBackColor = False
+        '
+        'BtnAdd
+        '
+        Me.BtnAdd.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.BtnAdd.Location = New System.Drawing.Point(115, 361)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(75, 32)
+        Me.BtnAdd.TabIndex = 3
+        Me.BtnAdd.Text = "Add"
+        Me.BtnAdd.UseVisualStyleBackColor = False
+        '
+        'BtnView
+        '
+        Me.BtnView.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.BtnView.Location = New System.Drawing.Point(344, 361)
+        Me.BtnView.Name = "BtnView"
+        Me.BtnView.Size = New System.Drawing.Size(75, 32)
+        Me.BtnView.TabIndex = 4
+        Me.BtnView.Text = "View"
+        Me.BtnView.UseVisualStyleBackColor = False
+        '
+        'BtnDelete
+        '
+        Me.BtnDelete.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.BtnDelete.Location = New System.Drawing.Point(449, 361)
+        Me.BtnDelete.Name = "BtnDelete"
+        Me.BtnDelete.Size = New System.Drawing.Size(75, 32)
+        Me.BtnDelete.TabIndex = 5
+        Me.BtnDelete.Text = "Delete"
+        Me.BtnDelete.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(134, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Add an app"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(129, 211)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(115, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Update an app"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(129, 153)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "View an app"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(129, 271)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(109, 20)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Delete an app"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(322, 93)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(190, 26)
+        Me.TextBox1.TabIndex = 10
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(322, 211)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(190, 26)
+        Me.TextBox3.TabIndex = 12
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(322, 271)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(190, 26)
+        Me.TextBox4.TabIndex = 13
+        '
+        'ListboxApps
+        '
+        Me.ListboxApps.FormattingEnabled = True
+        Me.ListboxApps.ItemHeight = 20
+        Me.ListboxApps.Location = New System.Drawing.Point(322, 135)
+        Me.ListboxApps.Name = "ListboxApps"
+        Me.ListboxApps.Size = New System.Drawing.Size(190, 64)
+        Me.ListboxApps.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gold
+        Me.ClientSize = New System.Drawing.Size(703, 515)
+        Me.Controls.Add(Me.ListboxApps)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BtnDelete)
+        Me.Controls.Add(Me.BtnView)
+        Me.Controls.Add(Me.BtnAdd)
+        Me.Controls.Add(Me.BtnUpdate)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents BtnUpdate As Button
+    Friend WithEvents BtnAdd As Button
+    Friend WithEvents BtnView As Button
+    Friend WithEvents BtnDelete As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents ListboxApps As ListBox
+End Class
---
+++ LabTask3Array/Form1.vb
@@ -0,0 +1,87 @@
+﻿'CONNIE KAWAS / 20DDT21F2030
+Public Class Form1
+    ' One-dimensional array to store app names
+    Dim apps(4) As String
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        Dim appName As String = InputBox("Enter the name of the app to add:", "Add App")
+
+        ' Check if the array is full
+        If apps.Length = apps.Count(Function(a) a IsNot Nothing) Then
+            MessageBox.Show("Error: The app list is full. Unable to add a new app.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Add the app to the first available slot
+        For i = 0 To apps.Length - 1
+            If apps(i) Is Nothing Then
+                apps(i) = appName
+                MessageBox.Show($"App '{appName}' added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Exit For
+            End If
+        Next
+
+    End Sub
+
+    Private Sub BtnUpdate_Click(sender As Object, e As EventArgs) Handles BtnUpdate.Click
+        Dim oldAppName As String = InputBox("Enter the name of the app to update:", "Update App")
+
+        ' Check if the app exists
+        If Not apps.Contains(oldAppName) Then
+            MessageBox.Show($"Error: App '{oldAppName}' not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Dim newAppName As String = InputBox($"Enter the new name for the app '{oldAppName}':", "Update App")
+
+        ' Update the app name
+        For i = 0 To apps.Length - 1
+            If apps(i) = oldAppName Then
+                apps(i) = newAppName
+                MessageBox.Show($"App '{oldAppName}' updated to '{newAppName}' successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Exit For
+            End If
+        Next
+
+    End Sub
+
+    Private Sub BtnView_Click(sender As Object, e As EventArgs) Handles BtnView.Click
+        ListboxApps.Items.Clear()
+
+        ' Display all installed apps in the listbox
+        For Each app In apps
+            If app IsNot Nothing Then
+                ListboxApps.Items.Add(app)
+            End If
+        Next
+    End Sub
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        Dim appNameToDelete As String = InputBox("Enter the name of the app to delete:", "Delete App")
+        ' Check if the app exists
+        If Not apps.Contains(appNameToDelete) Then
+            MessageBox.Show($"Error: App '{appNameToDelete}' not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Delete the app
+        For i = 0 To apps.Length - 1
+            If apps(i) = appNameToDelete Then
+                apps(i) = Nothing
+                MessageBox.Show($"App '{appNameToDelete}' deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Exit For
+            End If
+        Next
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize sample app names
+        apps(0) = "App1"
+        apps(1) = "App2"
+        apps(2) = "App3"
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Me.Close()
+    End Sub
+End Class
